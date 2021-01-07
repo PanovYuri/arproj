6bb3537018acf77ea48cae7d79fdf5131af74f6e
Panov Yuri <35775430+PanovYuri@users.noreply.github.com>
2021-01-07 19:09:19 +0500

    add toggle button

--- arsite/static/js/app.js
+++ arsite/static/js/app.js
@@ -1,1 +1,6 @@
-console.log("Hello, world!")
+function activeBurger() {
+    const burger = document.getElementsByClassName('navbar-burger')[0]
+    const menu = document.getElementsByClassName('navbar-menu')[0]
+    burger.classList.toggle('is-active')
+    menu.classList.toggle('is-active')
+}
